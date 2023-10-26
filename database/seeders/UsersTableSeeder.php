d0d463c44b2e57529e653978710e773cba26567f
riyanandri <hiddenfoxe01@gmail.com>
2023-10-27 04:45:37 +0700

    multiauth with breeze

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            
+            // admin
+            [
+                'name' => 'Admin Kampung Homestay',
+                'email' => 'kampunghomestay@gmail.com',
+                'password' => Hash::make('admin@123'),
+                'role' => 'admin',
+                'status' => 'active',
+            ],
+
+            // user
+            [
+                'name' => 'Customer',
+                'email' => 'customer@gmail.com',
+                'password' => Hash::make('customer@123'),
+                'role' => 'user',
+                'status' => 'active',
+            ],
+        ]);
+    }
+}
